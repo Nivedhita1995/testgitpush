f330b70817c6e815a4e26c41a5b1355ed5004da3
Nithi <nivedhita16.reddy@gmail.com>
2022-07-21 14:20:14 +0530

    this is my first code push in git ritu

---
+++ test1.py
@@ -0,0 +1,11 @@
+ d= {
+    "name":"rithu",
+    "email" : "rithu@ineuron.ai",
+    "surname" : "shetty"
+}
+d= {
+    "name":"rithu",
+    "email" : "rithu@ineuron.ai",
+    "surname" : "shetty"
+
+}
